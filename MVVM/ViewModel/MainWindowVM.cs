192478307e81322ecc96f84d427c90c99bf70c82
Vasiliy <wudgexyz@gmail.com>
2022-06-26 02:29:49 +0300

    Added MVVM structure; Added MainWindowVM class; Added ObservableObject wich implement INotifyPropertyChanged

---
+++ MVVM/ViewModel/MainWindowVM.cs
@@ -0,0 +1,19 @@
+﻿using eduClient.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eduClient.MVVM.ViewModel
+{
+    internal class MainWindowVM : ObservableObject
+    {
+
+
+        public MainWindowVM()
+        {
+
+        }
+    }
+}
