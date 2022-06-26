e691a998acd2d1032994c71d55a0d6bad8cd6ce7
Vasiliy <wudgexyz@gmail.com>
2022-06-26 14:23:13 +0300

    Edit mainwindow.xaml: Add:  -WindowStyle, ResizeMode, Transparency

--- MVVM/ViewModel/MainWindowVM.cs
+++ MVVM/ViewModel/MainWindowVM.cs
@@ -1,4 +1,5 @@
 ﻿using eduClient.Core;
+using eduClient.MVVM.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,10 +11,25 @@ namespace eduClient.MVVM.ViewModel
     internal class MainWindowVM : ObservableObject
     {
 
+        public RelayCommand TestButtonCommand { get; set; }
+        private string _textTest;
+
+        public string TestText
+        {
+            get { return _textTest; }
+            set { _textTest = value; NotifyPropertyChanged(); }
+        }
 
         public MainWindowVM()
         {
+            TestButtonCommand = new RelayCommand(o =>
+            {
+                MainWindowModel model = new MainWindowModel();
 
+                TestText = model.HelloWorld();
+
+
+            });
         }
     }
 }
